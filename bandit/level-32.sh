e22e69e52615d821974ce09f8864f07c630ebe4f
mohammadne <mohammadne.dev@gmail.com>
2022-06-30 19:03:14 +0430

    feat: complete uppershell

---
+++ bandit/level-32.sh
@@ -0,0 +1,6 @@
+# $0 is an environment variable that holds the shell command that is 
+# used to execute all the commands in current shell.
+>> $0
+
+$ cat /etc/bandit_pass/bandit33
+# c9c3199ddf4121b10cf581a98d51caee

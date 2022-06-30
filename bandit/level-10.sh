1e7167d85f1dc47d68a4740977e923c903373898
mohammadne <mohammadne.dev@gmail.com>
2022-06-30 11:26:05 +0430

    feat: complete level 11 & 12

---
+++ bandit/level-10.sh
@@ -0,0 +1,5 @@
+bandit10@bandit:~$ cat data.txt 
+# VGhlIHBhc3N3b3JkIGlzIElGdWt3S0dzRlc4TU9xM0lSRnFyeEUxaHhUTkViVVBSCg==
+
+bandit10@bandit:~$ cat data.txt | base64 -d
+# The password is IFukwKGsFW8MOq3IRFqrxE1hxTNEbUPR
---
+++ bandit/level-11.sh
@@ -0,0 +1,5 @@
+bandit11@bandit:~$ cat data.txt 
+# Gur cnffjbeq vf 5Gr8L4qetPEsPk8htqjhRK8XSP6x2RHh
+
+bandit11@bandit:~$ cat data.txt | tr '[a-zA-Z]' '[n-za-mN-ZA-M]'
+# The password is 5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu

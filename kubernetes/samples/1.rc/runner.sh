3bd99e470d9469ab4be01cde7b7d5e9ff39d3c15
mohammadne <mohammadne@mail.ru>
2022-08-24 22:46:56 +0430

    feat: add rc samples

---
+++ kubernetes/samples/1.rc/runner.sh
@@ -0,0 +1,10 @@
+kubectl create -f rcss.yml
+
+kubectl get pods
+kubectl get rc
+
+kubectl scale rc helloworld-rc --replicas=4
+kubectl delete pod <one-of-the-pods>
+kubectl get pods
+
+kubectl delete rc helloworld-rc 

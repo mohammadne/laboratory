2b656fb4a86251e205f954332e7bdc370a69f0a7
mohammadne <mohammadne@mail.ru>
2022-08-27 11:12:39 +0430

    feat: add service discovery 1th sample

---
+++ kubernetes/samples/10.service-discovery-1/runner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+kubectl run -it --image=busybox --rm=true testing-busybox -- sh
+
+# get actual IP of the pod
+hostname -I
+# 10.136.112.93
+
+# display the system's DNS name
+hostname 
+# testing-busybox
+
+# show dns-server or nameserver (option 1)
+cat /etc/resolv.conf 
+# search mohammadne-playground.svc.cluster.local svc.cluster.local cluster.local c.okteto-prod.internal
+# nameserver 172.30.0.10
+# options ndots:5
+
+# show dns-server or nameserver (option 2)
+nslookup google.com
+# Server:		172.30.0.10
+# Address:	172.30.0.10:53
+
+# show dns-server or nameserver (option 3)
+kubectl get service -n kube-system kube-dns
+# NAME       TYPE        CLUSTER-IP    EXTERNAL-IP   PORT(S)         AGE
+# kube-dns   ClusterIP   10.152.0.10   <none>        53/UDP,53/TCP   15d

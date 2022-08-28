fb0c4ff43d95e380be6847e0939d7098832b0d16
mohammadne <mohammadne@mail.ru>
2022-08-28 12:52:02 +0430

    feat: update ingress resources

--- kubernetes/samples/14.ingress/runner.sh
+++ kubernetes/samples/14.ingress/runner.sh
@@ -4,10 +4,10 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
 
 kubectl apply \
-    -f echoheaders.yml \
-    -f helloworld-v1.yml \
-    -f helloworld-v2.yml \
-    -f helloworld-ingress.yml
+    -f apple.yml \
+    -f banana.yml \
+    -f orange.yml \
+    -f fruits-ingress.yml
 
 kubectl describe ingress helloworld-ingress
 

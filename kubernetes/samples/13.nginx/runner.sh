1404f7fe4da28f5229b72f5cc71958032d88b804
mohammadne <mohammadne@mail.ru>
2022-08-28 10:36:55 +0430

    feat: update runner

--- kubernetes/samples/13.nginx/runner.sh
+++ kubernetes/samples/13.nginx/runner.sh
@@ -1,4 +1,6 @@
-#!/bib/bash
+#!/bin/bash
+
+# we will mount configMap data to nginx `/etc/nginx/conf.d` path to be used by nginx
 
 # create configMap (option 1)
 kubectl create configmap helloworld-nginx-config --from-file=reverseproxy.conf
@@ -9,10 +11,10 @@ kubectl create -f cm.yml
 # the result is the same
 kubectl get configmap helloworld-nginx-config -o yaml
 
-# create pod & service
+# create a pod with 2 containers on the same pod, one is our nodejs app and
+# another one is nginx reverse-proxy which will redirect traffic to port 80 of the pod
+# to port 3000 of nodejs app which means we add reverse-proxy in-front of the nodejs app
 kubectl create -f helloworld-nginx-pod.yml -f helloworld-nginx-service.yml
-
-
 
 kubectl exec -it helloworld-nginx-pod -c nginx -- bash
 > cat /etc/conf.d/reverseproxy.conf
